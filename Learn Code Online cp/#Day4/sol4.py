bd7383ac4e7b6d590819aaf81e5d70acbda5a067
kirtan-lab <62734474+kirtan-lab@users.noreply.github.com>
2020-10-12 22:04:17 +0530

    Add files via upload

---
+++ Learn Code Online cp/#Day4/sol4.py
@@ -0,0 +1,11 @@
+
+totalHD = 400
+totalHDperContainer = 8
+totalcontainer = 0
+while (totalHD >= totalHDperContainer):
+    totalHD -= totalHDperContainer
+    totalcontainer += 1
+     
+print("jack bu total",totalcontainer,"containers")
+
+
